68ccc14edfe01dbe52576fb43cd38d6922c1bf43
SKYICE01 <73750081+SKYICE01@users.noreply.github.com>
2021-10-11 21:27:57 +0200

    Add files via upload
    Help janco with Login to form.

---
+++ Janco_help_with_login/LocalLoginSystem/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LocalLoginSystem
+{
+    class User
+    {
+
+        string username;
+        string password;
+
+        public User(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+
+
+      /*  public readonly string[] mUsernames = { "username1", "username2", "username3" };
+
+
+        public readonly string[] mPasswords = { "password1", "password2", "password3" };
+
+        public readonly List<string> mUserList = new List<string>();
+
+
+        public readonly List<string> mPasswordList = new List<string>();*/ // in MAIN file private 
+
+
+
+        public override string ToString()
+        {
+            return string.Format($" {this.username} {this.password} \n");
+        }
+
+
+
+
+
+
+    }
+}
